6af481d8af64d87b6694f390a8d678299e2e2f61
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2022-12-25 15:00:20 +0300

    Domino Plling problem solution
    A2SV-Community ProgressSheet problem 4 solution

---
+++ dominoPiling.py
@@ -0,0 +1,12 @@
+# domino plling problem solution 
+
+# accept  value and extract them to variables
+
+m, n = input().split()
+
+# change values to intiger 
+n = int(n)
+m = int(m)
+
+total = int(m*n / 2)
+print(total)
