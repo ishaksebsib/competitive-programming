603441b8fc05f5ea02db3b0ee20c2821087389d7
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-08-13 07:07:27 +0300

    best problem of the day

---
+++ Array and Hashing/searchInsertPosition.py
@@ -0,0 +1,8 @@
+# LINK : https://leetcode.com/problems/search-insert-position/
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        for i,val in enumerate(nums):
+            if val >= target:
+                return i
+        return len(nums)
