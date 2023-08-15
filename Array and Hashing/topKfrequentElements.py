57cc771ac7030a56c1c966e252380dbb35966389
ishaksebsib <121272992+ishaksebsib@users.noreply.github.com>
2023-08-15 22:29:46 +0300

    needs update

---
+++ Array and Hashing/topKfrequentElements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = {}
+        for i in nums:
+            dic[i] = dic.get(i,0) + 1
+    
+        frequency = dict(sorted(dic.items(), key=lambda x: x[1], reverse=True))
+        result = list(frequency.keys())[:k]
+        
+        return result
+        
