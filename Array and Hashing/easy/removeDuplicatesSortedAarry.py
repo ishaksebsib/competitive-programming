2f6c5f5900dd2ca7e225367895570f59f78dddaf
ishaksebsib <ishaksebsib@gmail.com>
2023-10-31 20:40:49 +0300

    cool solution using pointers

---
+++ Array and Hashing/easy/removeDuplicatesSortedAarry.py
@@ -0,0 +1,9 @@
+# LINK : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l = 1
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[l] = nums[i]
+                l+=1
+        return l
