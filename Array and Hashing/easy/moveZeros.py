38fdcbabd14fed00bf770ae5a38a4a6f9bbcdca8
ishaksebsib <ishaksebsib@gmail.com>
2023-10-31 21:11:43 +0300

    modifide code

--- Array and Hashing/easy/moveZeros.py
+++ Array and Hashing/easy/moveZeros.py
@@ -4,7 +4,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in nums:
-            if i == 0:
-                nums.remove(i)
-                nums.append(i)
+        l = 0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+
+
+
+# MY WAY :) but it's not good
+
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         for i in nums:
+#             if i == 0:
+#                 nums.remove(i)
+#                 nums.append(i)
