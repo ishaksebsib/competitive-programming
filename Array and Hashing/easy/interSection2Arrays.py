38d46f659992801e3d9db03f8c22b25cb6b568a9
ishaksebsib <ishaksebsib@gmail.com>
2023-11-05 08:36:59 +0300

    modifid complexity

--- Array and Hashing/easy/interSection2Arrays.py
+++ Array and Hashing/easy/interSection2Arrays.py
@@ -1,5 +1,19 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        n1 = set(nums1)
+        n2 = set(nums2)
+        res = []
+
+        for i in n1:
+            if i in n2:
+                res.append(i)
+        
+        return res
+
+# old soulution 
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         res = []
 
         for i in nums1:
