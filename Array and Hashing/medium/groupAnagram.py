e130483513aa339a62bddd95ca6025656b6869f7
ishaksebsib <ishaksebsib@gmail.com>
2023-11-08 07:21:43 +0300

    updated version but the result is not efficent

--- Array and Hashing/medium/groupAnagram.py
+++ Array and Hashing/medium/groupAnagram.py
@@ -1,5 +1,17 @@
 # LINK : https://leetcode.com/problems/group-anagrams/description/ 
 
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mydic = defaultdict(list)
+
+        for i in strs:
+            count = [0] * 26
+            for j in i:
+                count[ord(j) - ord('a')] += 1
+                
+            mydic[str(count)].append(i)
+
+        return mydic.values()
 
 
 # my first version 
