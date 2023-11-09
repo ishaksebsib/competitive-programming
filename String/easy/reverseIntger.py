68377ff6a6aa07db46eecbb4638b5e6e66e9d909
ishaksebsib <ishaksebsib@gmail.com>
2023-11-09 09:31:25 +0300

    modifid complexity

--- String/easy/reverseIntger.py
+++ String/easy/reverseIntger.py
@@ -3,15 +3,12 @@
 # my first sluiton
 class Solution:
     def reverse(self, x: int) -> int:
-        h = 2**31 - 1
-        l = -2**31
-
         if x < 0:
             x = -int(str(abs(x))[::-1])
         else:
             x = int(str(x)[::-1])
         
-        if x > h or x < l:
+        if x.bit_length() >= 32:
             return 0
 
         return x
