24062d1cd099090b3d6c8da7bbed301242c6c331
ishaksebsib <ishaksebsib@gmail.com>
2023-11-13 13:51:53 +0300

    modifid complexity

--- String/easy/longestCommonPrefix.py
+++ String/easy/longestCommonPrefix.py
@@ -1,5 +1,24 @@
 # LINK : https://leetcode.com/problems/longest-common-prefix/description/
 
+# more efficent soution 
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        strs = sorted(strs)
+        l = strs[0]
+        r = strs[-1]
+        res = ""
+
+        for i in range(len(l)):
+            if l[i] == r[i]:
+                res+=l[i]
+                continue
+            return res
+
+        return res
+
+# my old solution 
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         expre = strs[0]
