0d876d1142fca3c5f2078cda6e55f9a408906d11
ishaksebsib <ishaksebsib@gmail.com>
2023-11-13 20:18:22 +0300

    my first linkedlist problem on leetcode

---
+++ LinkedList/medium/delteNodeInLinkedList.py
@@ -0,0 +1,6 @@
+# LINK : https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+class Solution:
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next
