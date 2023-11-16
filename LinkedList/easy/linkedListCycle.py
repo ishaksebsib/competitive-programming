a1c2053ff360fb277f1d240814e3a18f6e13b69a
ishaksebsib <ishaksebsib@gmail.com>
2023-11-16 11:34:19 +0300

    myFav

---
+++ LinkedList/easy/linkedListCycle.py
@@ -0,0 +1,30 @@
+# LINK : https://leetcode.com/problems/linked-list-cycle/
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                return True
+
+        return False
+
+
+# my way
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        is_exist = set()
+
+        while head:
+            if head in is_exist:
+                return True
+            is_exist.add(head)
+            head = head.next
+            
+        return False
