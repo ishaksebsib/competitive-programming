e91a59df55870257a5d6d896c617701b8112d4ab
ishaksebsib <ishaksebsib@gmail.com>
2023-11-25 22:54:18 +0300

    min stack solved

---
+++ 004 Stack/medium/minStack.py
@@ -0,0 +1,26 @@
+# LINK : https://leetcode.com/problems/min-stack/
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = []
+        
+
+    def push(self, val: int) -> None:
+        if not self.min:
+            self.min.append(val)
+        elif val <= self.getMin():
+            self.min.append(val)
+            
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.getMin() == self.top():
+            self.min.pop()
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
