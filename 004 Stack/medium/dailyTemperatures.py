d8eeb540782b9bba452a72236453c08a6bf85b50
ishaksebsib <ishaksebsib@gmail.com>
2023-11-28 20:54:11 +0300

    i don't get it

---
+++ 004 Stack/medium/dailyTemperatures.py
@@ -0,0 +1,15 @@
+# LINK : https://leetcode.com/problems/daily-temperatures/
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        n = len(temperatures)
+        res = [0] * n
+
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                index = stack.pop()
+                res[index] = i - index
+            stack.append(i)
+
+        return res
