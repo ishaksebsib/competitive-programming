4b2be203f5a7013ccc1150cc961a11066d676b98
ishaksebsib <ishaksebsib@gmail.com>
2023-11-30 20:50:12 +0300

    good problem

---
+++ 004 Stack/easy/finalPriceWithDiscount.py
@@ -0,0 +1,14 @@
+# LINK : https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
+
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+        
+        for i in range(len(prices)):
+            stack.append(prices[i])
+            for j in range(i+1,len(prices)):
+                if prices[j] <= prices[i]:
+                    stack[-1] = prices[i] - prices[j]
+                    break
+                    
+        return stack
