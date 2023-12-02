ec0dfba1b8a2b3225a45bb0ae0899572570f5357
ishaksebsib <ishaksebsib@gmail.com>
2023-12-02 20:33:33 +0300

    i happy to make it happen

---
+++ 004 Stack/medium/evaluateReversePolishNot.py
@@ -0,0 +1,23 @@
+# LINK : https://leetcode.com/problems/evaluate-reverse-polish-notation/
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for i in tokens:
+            if i == "+":
+                b,a = stack.pop(), stack.pop()
+                stack.append(a+b)
+            elif i == "-":
+                b,a = stack.pop(), stack.pop()
+                stack.append(a-b)
+            elif i == "*":
+                b,a = stack.pop(), stack.pop()
+                stack.append(a*b)
+            elif i == "/":
+                b,a = stack.pop(), stack.pop()
+                stack.append(int(a/b))
+            else:
+                stack.append(int(i))
+
+        return stack[0]
